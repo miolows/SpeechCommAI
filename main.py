1e4a1f4d019714d54c9736e0a77d05e0fe3deb46
Michal Olowski <olowskimi@outlook.com>
2022-07-10 18:03:23 +0200

    deleted learning flag

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ from wrap import timer
 if __name__ == '__main__':
     
     config = Configurator()
-    data_collection = 'first 2'
-    ai = AudioAI(config, data_collection, True)
+    data_collection = 'all'
+    ai = AudioAI(config, data_collection)
     
     live_record(config, ai)
-    
-   
